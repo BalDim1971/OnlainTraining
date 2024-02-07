1285f103f7148827fa66078a0a48982eee8668cd
Дмитрий Балашов <korochun@yandex.ru>
2024-02-07 14:45:04 +0300

    Курс "DRF" Домашние работы Домашнее задание по уроку 24.1 "Вьюсеты и дженерики" 1. Добавлен пакет autopep8 2. Файлы пропущены через autopep8

--- lms/models.py
+++ lms/models.py
@@ -10,14 +10,17 @@ class Course(models.Model):
     """
     Модель курса обучения
     """
-    
+
     name = models.CharField(max_length=100, verbose_name='наименование')
-    preview = models.ImageField(upload_to='course_previews/', verbose_name='картинка', **NULLABLE)
+    preview = models.ImageField(
+        upload_to='course_previews/',
+        verbose_name='картинка',
+        **NULLABLE)
     description = models.TextField(verbose_name='описание')
-    
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name = 'курс'
         verbose_name_plural = 'курсы'
@@ -28,16 +31,22 @@ class Lesson(models.Model):
     """
     Модель урока курса обучения
     """
-    
+
     name = models.CharField(max_length=100, verbose_name='название урока')
-    preview = models.ImageField(upload_to='lesson_previews/', verbose_name='картинка', **NULLABLE)
+    preview = models.ImageField(
+        upload_to='lesson_previews/',
+        verbose_name='картинка',
+        **NULLABLE)
     description = models.TextField(verbose_name='описание')
     video_url = models.URLField(verbose_name='видеоурок', **NULLABLE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='курс')
-    
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='курс')
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
