ae15e8910e5cb44b04a250985525b0b444928f0a
BalashovDV <korochun@yandex.ru>
2024-02-07 06:18:01 +0300

    Онлайн-обучение. Курс "DRF" Домашние работы Домашнее задание по уроку 24.1 "Вьюсеты и дженерики" Переставил import

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404
-from users.serializers import UserSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
 
 from users.models import User
+from users.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ViewSet):
