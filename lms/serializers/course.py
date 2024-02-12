d2baafffa0e3ef0b872992aea18f490ebc7da7d7
BalashovDV <korochun@yandex.ru>
2024-02-12 06:20:45 +0300

    Онлайн-обучение. Курс "DRF" Домашние работы Домашнее задание по уроку 24.2 "Вьюсеты и дженерики" Попытка добавить вычисление количества уроков в курсе

--- lms/serializers/course.py
+++ lms/serializers/course.py
@@ -1,9 +1,22 @@
+"""
+Сериаизаторы для класса Курс
+"""
+
 from rest_framework import serializers
 
 from lms.models import Course
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    """
+    Класс-сериализатор для курса.
+    """
+
+    count_lesson = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_count_lesson(self, obj):
+        return obj.lesson.count()
