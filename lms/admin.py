1becf0c76bfec907ec72152556ef0a20949d6e8c
BalashovDV <korochun@yandex.ru>
2024-02-14 18:09:41 +0300

    Онлайн-обучение. Курс "DRF" Домашние работы Домашнее задание по уроку 24.2 "Сериализаторы" Добавил админку для первых Курса и Уроков. Добавил скрипт создания суперпользователя.

--- lms/admin.py
+++ lms/admin.py
@@ -1,3 +1,16 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from lms.models import Course, Lesson
+
+
+@admin.register(Course)
+class HabitsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'preview', 'description',)
+    list_filter = ('name',)
+
+
+@admin.register(Lesson)
+class HabitsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'preview', 'description', 'video_url',
+                    'course',)
+    list_filter = ('name', 'course',)
