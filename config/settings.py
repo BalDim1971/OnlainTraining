d63bf5c80a7fe705507437fb7ca2f9cb97bc11dc
BalashovDV <korochun@yandex.ru>
2024-03-06 19:22:06 +0300

    Онлайн-обучение. Курс "DRF" Домашние работы Домашнее задание по уроку 26.1 "Документирование и безопасность" 1. settings.py косметические изменения. 2. Добавлен файл для проведения оплаты.

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,7 @@ import os
 
 from dotenv import load_dotenv
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -148,15 +149,15 @@ AUTH_USER_MODEL = 'users.User'
 
 # Настройки JWT-токенов
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    ],
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny'
-    ],
+    ),
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 4,
